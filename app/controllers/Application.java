2390f4d02ce1c20d6fa2d46e1cf2fdb1d78e9130
Aidar Babanov <aidar.babanov>
2017-03-31 03:54:15 +0600

    Test 1

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,11 +6,10 @@ import play.mvc.*;
 import java.util.*;
 
 import models.*;
-
 public class Application extends Controller {
 
     public static void index() {
         render();
     }
-
+    //Test
 }
